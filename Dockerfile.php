344ce668fd1bfb7ef5ece1e6aea049629ce7da8a
Ivan Grynenko <ivan.grynenko@gmail.com>
2020-07-08 11:19:16 +1000

    Added cache directory.

--- Dockerfile.php
+++ Dockerfile.php
@@ -3,6 +3,9 @@ FROM ${CLI_IMAGE:-builder} as builder
 
 FROM amazeeio/php:7.2-fpm
 
+# Adding caching directory.
+RUN mkdir -p /app/web/content/cache/tmp && fix-permissions /app/web/content/cache/tmp
+
 # Create temporary wordpress log file.
 RUN touch /tmp/wp-errors.log && fix-permissions /tmp/wp-errors.log
 
