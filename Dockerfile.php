76f8798248cd37ef24012daca6cdf1d67e24aacd
Ivan Grynenko <ivan.grynenko@gmail.com>
2020-07-08 10:28:51 +1000

    Added error handling to activate error log.

--- Dockerfile.php
+++ Dockerfile.php
@@ -3,4 +3,7 @@ FROM ${CLI_IMAGE:-builder} as builder
 
 FROM amazeeio/php:7.2-fpm
 
+# Create temporary wordpress log file.
+RUN touch /tmp/wp-errors.log && fix-permissions /tmp/wp-errors.log
+
 COPY --from=builder /app /app
