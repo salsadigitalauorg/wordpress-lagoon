e5427548127831f4158e40880c14b258494efb9e
Ivan Grynenko <ivan.grynenko@gmail.com>
2021-01-30 11:44:34 +1100

    Added WP Lagoon Logs.

--- web/content/mu-plugins/load.php
+++ web/content/mu-plugins/load.php
@@ -3,3 +3,7 @@
 if (file_exists(WPMU_PLUGIN_DIR.'/disable-wp-rest-api/disable-wp-rest-api.php')) {
   require WPMU_PLUGIN_DIR.'/disable-wp-rest-api/disable-wp-rest-api.php';
 }
+
+if (file_exists(WPMU_PLUGIN_DIR.'/wp_lagoon_logs/wp_lagoon_logs.php')) {
+  require WPMU_PLUGIN_DIR.'/wp_lagoon_logs/wp_lagoon_logs.php';
+}
