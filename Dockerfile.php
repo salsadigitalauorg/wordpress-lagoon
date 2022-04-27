14a0ee00c4ee2a8601f11eca884de5d8b3862211
Ivan Grynenko <ivan.grynenko@gmail.com>
2022-04-27 06:53:01 +0300

    Updated base image and added chmod.

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,9 +1,12 @@
 ARG CLI_IMAGE
 FROM salsadigital/wordpress-lagoon-cli:latest as builder
 
-FROM uselagoon/php:7.4-fpm
+FROM uselagoon/php-7.4-fpm
 
 # Create temporary wordpress log file.
 RUN touch /tmp/wp-errors.log && fix-permissions /tmp/wp-errors.log
 
 COPY --from=builder /app /app
+
+# Make docroot read-only.
+RUN chmod -R a-w /app/web; chmod -R a-w /app/vendor
