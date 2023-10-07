21bafbfe0a4e394ef7bb002ead771aa1beb137e3
Ivan Grynenko <ivan.grynenko@gmail.com>
2023-10-07 09:59:05 +0300

    Switched master branch to PHP 8.1

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -1,11 +1,12 @@
 ARG CLI_IMAGE
-FROM salsadigital/wordpress-lagoon-cli:latest as builder
 
-FROM uselagoon/php-8.0-fpm
+FROM ${CLI_IMAGE} as cli
+
+FROM uselagoon/php-8.1-fpm
 
 RUN apk add --no-cache tzdata
 
 # Create temporary wordpress log file.
 RUN touch /tmp/wp-errors.log && fix-permissions /tmp/wp-errors.log
 
-COPY --from=builder /app /app
+COPY --from=cli /app /app
